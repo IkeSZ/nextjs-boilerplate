4a4e019b870afcb10bda3797fee3ebe611dfa476
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-01-30 12:29:17 -0300

    Added React Testing Library configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom', // Consegue simular o DOM do Browser para testes
-  testPathIgnore: ['/node_modules/', '/.next'], // Arquivos que o Jest ignora
-  collectCoverageTrue: true, // Faz o teste de cobertura
+  testPathIgnorePatterns: ['/node_modules/', '/.next'], // Arquivos que o Jest ignora
+  collectCoverage: true, // Faz o teste de cobertura
   collectCoverageFrom: ['src/**/*.ts(x)'],
   // Arquivos que precisam estar no coverage, no caso, todos dentro da pasta "src" que terminam em "ts" ou "tsx"
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Outras configs
---
+++ src/components/main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Ednaldo Pereira</h1>
+    </main>
+  );
+};
+
+export default Main;
