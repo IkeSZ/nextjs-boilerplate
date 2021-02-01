af77fc6a2e245938681232c82e7062b9020e8f9a
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-02-01 15:38:50 -0300

    Fixing some HBS templates

--- src/components/Button/test.spec.tsx
+++ src/components/Button/test.spec.tsx
@@ -2,4 +2,16 @@ import { screen, render } from '@testing-library/react';
 
 import Button from './index';
 
-describe('<Button />', () => {});
+describe('<Button />', () => {
+  it('should be able to render the heading', () => {
+    const { container } = render(<Button />);
+
+    expect(
+      screen.getByRole('heading', {
+        name: 'Button',
+      })
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
